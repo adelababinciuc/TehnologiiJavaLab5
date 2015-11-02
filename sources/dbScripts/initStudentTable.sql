7f55fac7eab5194a5ee5ddfca680d737a2bc3538
Adela Babinciuc <babinciuc.adela@gmail.com>
2015-11-02 22:19:46 +0200

    Added sql file for student

--- sources/dbScripts/initStudentTable.sql
+++ sources/dbScripts/initStudentTable.sql
@@ -1,30 +1,7 @@
---TO DO
---Ruleaza: 1. CREATE TABLE, 2. CREATE SEQUENCE, 3. CREATE FUNCTION, 4. CREATE TRIGGER
-
-DROP TABLE student;
-
-CREATE TABLE student(student_id varchar(45), name varchar(45), cnp varchar(45), placeOfBirth varchar(45));
-
---INSERT INTO student(student_id, name , cnp, placeOfBirth) VALUES ('1', 'ana', '123', 'fcs');
-
-SELECT * FROM student;
-
-CREATE SEQUENCE STUDENT_SEQ
-   START WITH 1
-   MAXVALUE 9999999
-   MINVALUE 1;
-
-CREATE OR REPLACE FUNCTION GetNextStudent()
-   RETURNS "trigger" AS
-   $BODY$
-   BEGIN
-     New.student_id:=nextval('STUDENT_SEQ');
-     Return NEW;
-   END;
-   $BODY$
-   LANGUAGE 'plpgsql' VOLATILE;
- 
-CREATE TRIGGER "STUDENT_SEQ_NEXT"  
-  BEFORE INSERT ON "student"              
-  FOR EACH ROW
-  EXECUTE PROCEDURE GetNextStudent();
+CREATE TABLE student
+(
+student_id SERIAL PRIMARY KEY, 
+name varchar(45), 
+cnp varchar(45), 
+placeOfBirth varchar(45)
+);
